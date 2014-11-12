6c17a042825a9753823c83c0a704d43c500669f4
Jason <lewrisjj@gmail.com>
2014-11-12 00:49:51 -0500

    moving pollutantmean homework to the repository

---
+++ pollutantmeandatascience.R
@@ -0,0 +1,6 @@
+pollutantmean <- function(directory, pollutant, id = 1:332) {
+    data = lapply(id, function(i) read.csv(paste("/Users/BossDawg/Desktop/",directory,"/",formatC(i, 
+        width = 3, flag = "0"), ".csv", sep = ""))[[pollutant]])
+
+    return(mean(unlist(data), na.rm = TRUE))
+}
